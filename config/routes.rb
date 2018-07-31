ce83064e78d67a50fafa9d7a4174953c38f78ca4
TheFSilver <dasylva.f@gmail.com>
2018-07-31 19:27:11 +0200

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-  root to: 'static_pages#home'
+
+  get '/', to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
   get 'about/francois', to: 'static_pages#francois'
   get 'about/ani', to: 'static_pages#ani'
   get 'about/titouan', to: 'static_pages#titouan'
   get 'about/JR', to: 'static_pages#JR'
+
   get '/new', to: 'users#new'
+  get '/form_tag', to: 'users#form_tag'
+  post '/form_tag', to: 'users#create2'
   post '/new', to: 'users#create'
+  get '/form_for', to: 'users#form_for'
+  post '/form_for', to: 'users#create3'
   get '/error', to: 'users#error', as: 'error'
   get '/error2', to: 'users#error2', as: 'error2'
   get '/users/:username', to: 'users#show', as: 'show'
